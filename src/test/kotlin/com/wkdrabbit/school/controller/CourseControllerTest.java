a215bf804ba2c5754402a4407620a51b2021e3cd
BL-CoderOvertime <brandonlively@live.com>
2019-06-17 18:44:56 -0400

    Created the test classes

---
+++ src/test/kotlin/com/wkdrabbit/school/controller/CourseControllerTest.java
@@ -0,0 +1,30 @@
+package com.wkdrabbit.school.controller;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CourseControllerTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void listAllCourses() {
+    }
+
+    @Test
+    public void getCountStudentsInCourses() {
+    }
+
+    @Test
+    public void deleteCourseById() {
+    }
+}
